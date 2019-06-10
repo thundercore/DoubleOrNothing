fdec30d75c97dde05208e81d93c2a9f639003887
Yifan Jiang <yifan.jiang@thundertoken.com>
2019-06-10 15:44:12 -0700

    add build folder

---
+++ frontend/build/precache-manifest.7d534edafebf5a7a456b5eef5d87349a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b26cd33b532c51809bb5ded9312412",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59cdab8b5061b93680a8",
+    "url": "/static/css/main.146d7d5d.chunk.css"
+  },
+  {
+    "revision": "e5a70cece5fd0e52914a",
+    "url": "/static/js/2.a8423612.chunk.js"
+  },
+  {
+    "revision": "59cdab8b5061b93680a8",
+    "url": "/static/js/main.2b3af627.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d74eae6b6da4142fc847812c60ae3d5f",
+    "url": "/static/media/hurt.d74eae6b.svg"
+  },
+  {
+    "revision": "4b0217804ee24a398c8de3d184b884e3",
+    "url": "/static/media/icon_TT.4b021780.svg"
+  }
+]);
