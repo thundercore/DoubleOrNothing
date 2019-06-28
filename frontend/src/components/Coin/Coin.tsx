daffdc7ffa3fc709d53187dbc1b3f8d2bd0b8072
Yifan Jiang <yifan.jiang@thundertoken.com>
2019-06-28 16:16:39 -0700

    improve styling for coin flipping animation

--- frontend/src/components/Coin/Coin.tsx
+++ frontend/src/components/Coin/Coin.tsx
@@ -13,8 +13,14 @@ export class Coin extends React.PureComponent<ICoinProps> {
       >
         <div className="disk-body" />
         <div className="disk-body back" />
-        <div className="top-face top-back-face disk" />
-        <div className="disk face bottom-back-face" />
+        <div className="top-face top-back-face disk">
+          <div className="half-face half-face-lower" />
+          <div className="half-face half-face-upper" />
+        </div>
+        <div className="disk face bottom-back-face">
+          <div className="half-face half-face-lower" />
+          <div className="half-face half-face-upper" />
+        </div>
         <div className="disk bottom-front-face" />
         <div className="face top-face top-face-front disk" />
       </div>
