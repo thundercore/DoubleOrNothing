bb4a2c5a29d78012bd6f74765a2d785329405560
Yifan Jiang <yifan.jiang@thundertoken.com>
2019-07-04 10:51:51 -0700

    integrate referral lib

--- smart-contracts/contracts/DoubleOrNothing.sol
+++ smart-contracts/contracts/DoubleOrNothing.sol
@@ -1,17 +1,37 @@
-pragma solidity 0.4.25;
+pragma solidity 0.5.0;
 
-contract DoubleOrNothing {
-  address public owner;
+import '@thundercore/referral-solidity/contracts/Referral.sol';
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
+contract DoubleOrNothing is Ownable, Referral {
   event BetSettled(address player, uint256 winnings);
 
-  constructor() public {
-    owner = msg.sender;
-  }
+  constructor(
+    uint256[] memory _levelRate,
+    uint _referralBonus,
+    uint _decimals,
+    uint _secondsUntilInactive,
+    bool _onlyRewardActiveReferrers,
+    uint256[] memory _refereeBonusRateMap
+  ) Referral(
+      _levelRate,
+      _referralBonus,
+      _decimals,
+      _secondsUntilInactive,
+      _onlyRewardActiveReferrers,
+      _refereeBonusRateMap
+  ) public {}
 
   function() external payable {}
 
-  function bet() payable external {
+  function bet(address payable _referrer) payable external {
+    if(!hasReferrer(msg.sender)) {
+      addReferrer(_referrer);
+    }
+    bet();
+  }
+
+  function bet() payable public {
     // msg.value is added to the balance to begin with so you need to double it
     require(msg.value * 2 <= address(this).balance, 'Balance too low!');
     uint256 winnings = 0;
@@ -21,12 +41,11 @@ contract DoubleOrNothing {
       winnings = msg.value * 2;
       address(msg.sender).transfer(winnings);
     }
-
+    payReferral(msg.value);
     emit BetSettled(msg.sender, winnings);
   }
 
-  function withdraw(uint256 _amount) external {
-    require(msg.sender == owner, 'Only owner allowed to withdraw');
+  function withdraw(uint256 _amount) external onlyOwner {
     require(_amount <= address(this).balance, 'Balance too low!');
     address(msg.sender).transfer(_amount);
   }
--- smart-contracts/migrations/1_initial_migration.js
+++ smart-contracts/migrations/1_initial_migration.js
@@ -1,7 +1,14 @@
-var Migrations = artifacts.require("./Migrations.sol");
-var Double = artifacts.require("./DoubleOrNothing.sol");
+var Migrations = artifacts.require('./Migrations.sol')
+var Double = artifacts.require('./DoubleOrNothing.sol')
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
-  deployer.deploy(Double);
-};
+  deployer.deploy(Migrations)
+  deployer.deploy(Double, [300, 200, 100], 30, 1000, 86400, true, [
+    1,
+    500,
+    5,
+    750,
+    25,
+    1000
+  ])
+}
